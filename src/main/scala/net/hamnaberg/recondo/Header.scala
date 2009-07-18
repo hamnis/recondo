927dbaba3c6cf27baf1462d94476265b2616edde
Erlend Hamnaberg <ngarthl@gmail.com>
2009-07-18 19:10:22 +0200

    - Added parsing of directives.

--- src/main/scala/net/hamnaberg/recondo/Header.scala
+++ src/main/scala/net/hamnaberg/recondo/Header.scala
@@ -8,11 +8,26 @@ package net.hamnaberg.recondo
  * @version $Revision: #5 $ $Date: 2008/09/15 $
  */
 case class Header(name : String, value : String) {
+  require(name != null)
+  require(value != null)
+  val directives : Map[String, String] = parseValue(value)
+  
   override def toString() = name + ": " + value
+
+  def parseValue(value : String) : Map[String, String] = {
+    val foo = value.split(",").toList map {
+      x => {
+        val trimmed = x.trim
+        val directiveparts = trimmed.split("=", 2)
+        if (directiveparts.length == 2) directiveparts(0) -> directiveparts(1) else directiveparts(0) -> null
+      }
+    }
+    Map() ++ foo
+  }
 }
 
 object Header {
-  def apply(tuple : Tuple2[String, String]) {
+  def apply(tuple : Tuple2[String, String]) = {
     new Header(tuple._1, tuple._2)
   }
 }
---
+++ src/test/scala/net/hamnaberg/recondo/HeaderTest.scala
@@ -0,0 +1,35 @@
+package net.hamnaberg.recondo
+
+
+import org.junit.{Assert, Test}
+
+/**
+ * @author <a href="mailto:erlend@escenic.com">Erlend Hamnaberg</a>
+ * @version $Revision : $
+ */
+
+class HeaderTest {
+  @Test
+  def testSimpleHeader {
+    val header = new Header("foo", "foo")
+    Assert.assertEquals(header, Header(Tuple2("foo", "foo")));
+    Assert.assertEquals(header, Header("foo" -> "foo"));
+  }
+
+  @Test
+  def testSimpleHeaderDirectives {
+    val header = new Header("foo", "foo")
+    Assert.assertEquals(header, Header(Tuple2("foo", "foo")));
+    Assert.assertEquals(header, Header("foo" -> "foo"));
+    Assert.assertEquals(header.directives, Map("foo" -> null))
+  }
+
+  @Test
+  def testComplexHeaderDirectives {
+    val headerValue = "private,max-age=12, s-max-age=213, snabel"
+    val header = new Header("Cache-Control", headerValue)
+    Assert.assertEquals(header, Header(Tuple2("Cache-Control", headerValue)));
+    Assert.assertEquals(header, Header("Cache-Control" -> headerValue));
+    Assert.assertEquals(header.directives, Map("private" -> null, "max-age" -> "12", "s-max-age" -> "213", "snabel" -> null))
+  }
+}
