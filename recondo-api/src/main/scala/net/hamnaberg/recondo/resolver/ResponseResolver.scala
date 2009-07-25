c181a2f5c9794eadb1a895617801ce722926a277
Erlend Hamnaberg <ngarthl@gmail.com>
2009-07-25 18:37:01 +0200

    - Response resolver now declares that it throws IOExceptions

--- recondo-api/src/main/scala/net/hamnaberg/recondo/resolver/ResponseResolver.scala
+++ recondo-api/src/main/scala/net/hamnaberg/recondo/resolver/ResponseResolver.scala
@@ -1,11 +1,13 @@
 package net.hamnaberg.recondo.resolver
 
 
+import java.io.IOException
 
 /**
  * @author <a href="mailto:erlend@hamnaberg.net">Erlend Hamnaberg</a>
  * @version $Revision: #5 $ $Date: 2008/09/15 $
  */
 trait ResponseResolver {
+  @throws(classOf[IOException])
   def resolve(request : Request) : Response;
 }
