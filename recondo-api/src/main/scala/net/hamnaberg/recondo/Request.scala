a3bed107ff0410e0cb489a666b51c73514cec30d
Erlend Hamnaberg <ngarthl@gmail.com>
2009-10-06 16:55:17 +0200

    - All headers. - Added missing preferences from request

--- recondo-api/src/main/scala/net/hamnaberg/recondo/Request.scala
+++ recondo-api/src/main/scala/net/hamnaberg/recondo/Request.scala
@@ -7,33 +7,41 @@ import java.net.URI
  * @author <a href="mailto:erlend@hamnaberg.net">Erlend Hamnaberg</a>
  * @version $Revision : $
  */
-case class Request private[recondo](uri: URI,
-                                    method: Method,
-                                    headers: Headers,
-                                    conditionals: Conditionals,
-                                    credentials: Option[Credentials],
-                                    payload: Option[Payload]
+case class Request(uri: URI,
+                   method: Method,
+                   headers: Headers,
+                   conditionals: Conditionals,
+                   preferences: Preferences,
+                   credentials: Option[Credentials],
+                   payload: Option[Payload]
         ) extends PayloadContainer {
 
-  def headers(h: Headers): Request = {
-    new Request(uri, method, headers, conditionals, credentials, payload)
+  def headers(headers: Headers): Request = {
+    new Request(uri, method, headers, conditionals, preferences, credentials, payload)
   }
 
-  def conditionals(c: Conditionals): Request = {
-    new Request(uri, method, headers, c, credentials, payload)
+  private[recondo] def allHeaders() = {
+    val h  = headers ++ conditionals.toHeaders ++ preferences.toHeaders
+    if (hasPayload) {
+      val heads = h - HeaderConstants.CONTENT_TYPE
+      heads + payload.map{p => new Header(HeaderConstants.CONTENT_TYPE, p.MIMEType.toString)}.getOrElse(error("No payload!?!"))
+    }
+    else {
+      h
+    }
   }
 
-  def credentials(c: Credentials):Request = {
-    new Request(uri, method, headers, conditionals, Some(c), payload)
-  }
+  def conditionals(conditionals: Conditionals):Request = new Request(uri, method, headers, conditionals, preferences, credentials, payload)
 
-  def payload(p: Payload) : Request = {
-    new Request(uri, method, headers, conditionals, credentials, Some(p))
-  }  
+  def preferences(preferences: Preferences):Request = new Request(uri, method, headers, conditionals, preferences, credentials, payload)
+
+  def credentials(credentials: Credentials):Request = new Request(uri, method, headers, conditionals, preferences, Some(credentials), payload)
+
+  def payload(payload: Payload):Request = new Request(uri, method, headers, conditionals, preferences, credentials, Some(payload))
 }
 
 object Request {
-  def apply(requestURI: URI) = new Request(requestURI, Method.GET, Headers(), Conditionals(), None, None)
+  def apply(requestURI: URI) = new Request(requestURI, Method.GET, Headers(), Conditionals(), Preferences(), None, None)
 
   def apply(requestURI: String) : Request = apply(URI.create(requestURI))
 }
