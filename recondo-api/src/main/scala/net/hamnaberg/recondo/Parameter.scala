1d67da13ca5dea4c51c5e241f0074e4395c33108
Erlend Hamnaberg <ngarthl@gmail.com>
2009-10-13 18:42:41 +0200

    - apply return correct value

--- recondo-api/src/main/scala/net/hamnaberg/recondo/Parameter.scala
+++ recondo-api/src/main/scala/net/hamnaberg/recondo/Parameter.scala
@@ -5,13 +5,10 @@ package net.hamnaberg.recondo
  * @author <a href="mailto:erlend@hamnaberg.net">Erlend Hamnaberg</a>
  * @version $Revision: #5 $ $Date: 2008/09/15 $
  */
-
 case class Parameter(name : String, value : String) {
   override def toString = name + " " + value
 }
 
 object Parameter {
-  def apply(tuple : Tuple2[String, String]) {
-    new Parameter(tuple._1, tuple._2)
-  }
+  def apply(tuple : Tuple2[String, String]) = new Parameter(tuple._1, tuple._2)
 }
