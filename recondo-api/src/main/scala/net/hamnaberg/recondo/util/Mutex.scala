cda5b7ca9786c068ce94c9634a3bd186d4c91994
Erlend Hamnaberg <ngarthl@gmail.com>
2009-12-22 21:52:14 +0100

    - Added Mutex class from HttpCache4j

---
+++ recondo-api/src/main/scala/net/hamnaberg/recondo/util/Mutex.scala
@@ -0,0 +1,44 @@
+package net.hamnaberg.recondo.util
+
+import java.util.concurrent.locks.ReentrantLock
+
+/**
+ * @author <a href="mailto:erlend@hamnaberg.net">Erlend Hamnaberg</a>
+ * @version $Revision : $
+ */
+class Mutex[T] {
+  private var locks = Set[T]()
+  private val lock = new ReentrantLock
+  private val condition = lock.newCondition
+
+  def acquire(obj: T) {
+    lock.lock
+    try {
+      while (locks.contains(obj)) {
+        try {
+          condition.await
+        }
+        catch {
+          case e: InterruptedException => Thread.currentThread.interrupt
+        }
+        locks += obj
+      }
+    }
+    finally {
+      lock.unlock
+    }
+  }
+
+  def release(obj: T) {
+    lock.lock
+    try {
+      if (locks.contains(obj)) {
+        locks -= obj
+        condition.signal
+      }
+    }
+    finally {
+      lock.unlock
+    }
+  }
+}
